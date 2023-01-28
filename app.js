524ddad55c8a8379aa48e0b3f2814c6d7cc31b71
chairil ashar <asharchairil4@gmail.com>
2023-01-28 13:02:10 +0700

    fetching single product

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const User = require("./models/user");
+// const User = require("./models/user");
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("63d3354e4051a6651b30acb1")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("63d3354e4051a6651b30acb1")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -34,7 +34,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://Chairilashar:w2pqIwLjtLu0Ekqf@cluster0.dccacst.mongodb.net/test"
+    "mongodb+srv://Chairilashar:w2pqIwLjtLu0Ekqf@cluster0.dccacst.mongodb.net/shops"
   )
   .then((result) => {
     app.listen(3000);
