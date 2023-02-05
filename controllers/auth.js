12c0fe9341a3bd9387107454c4857bd2bb11a5d0
chairil ashar <asharchairil4@gmail.com>
2023-02-05 12:54:41 +0700

    sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,18 @@
+const user = require('../models/user');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'signup',
     isAuthenticated: false
   });
 };
@@ -21,9 +30,11 @@ exports.postLogin = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
+exports.postSignup = (req, res, next) => {};
+
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
     console.log(err);
     res.redirect('/');
-  });
-};
+  })
+}
