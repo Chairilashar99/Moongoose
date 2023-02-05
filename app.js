6000af9ff28006fbfd6c5f8416d621959650a7c1
chairil ashar <asharchairil4@gmail.com>
2023-02-05 14:27:03 +0700

    authentication

--- app.js
+++ app.js
@@ -59,18 +59,6 @@ mongoose.set("strictQuery", false);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,40 +1,62 @@
-const user = require('../models/user');
-const User = require('../models/user');
+const user = require("../models/user");
+const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: false
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
   });
 };
 
 exports.getSignup = (req, res, next) => {
-  res.render('auth/signup', {
-    path: '/signup',
-    pageTitle: 'signup',
-    isAuthenticated: false
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "signup",
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('63d4ffca5b8030d25c8727ab')
-    .then(user => {
+  User.findById("63d4ffca5b8030d25c8727ab")
+    .then((user) => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      req.session.save(err => {
+      req.session.save((err) => {
         console.log(err);
-        res.redirect('/');
+        res.redirect("/");
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if(userDoc) {
+        return res.redirect('/signup');
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: []}
+      })
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     console.log(err);
-    res.redirect('/');
-  })
-}
+    res.redirect("/");
+  });
+};
